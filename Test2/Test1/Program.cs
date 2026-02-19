dbcb6e7b90f72721d6cf085ab940a1f7b0b8d319
cromanenkopavlo-hub <c.romanenko.pavlo@student.uzhnu.edu.ua>
2026-02-19 23:56:43 +0200

    Test 1.2

---
+++ Test2/Test1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Test1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string[] student = { "Романенко", "Яльч","Скатов", "Панющик", "Бурч", "Німець", "Чаварга", "Куцик", "Левчинко", "Бурикін","Савко", "Губський", "Дронов", "Куцик" };
+                Random rnd = new Random();
+                List<string> used = new List<string>(); 
+                Console.WriteLine("Виведення 35 разів:");
+                for (int i = 1; i <= 35; i++)
+                {
+                    int A = rnd.Next(0, student.Length);
+                    string randomStudent = student[A];
+                    Console.WriteLine(student[A]);
+                    if (!used.Contains(randomStudent))
+                    {
+                        Console.WriteLine("Вибрано студента: " + randomStudent);
+                        used.Add(randomStudent);
+                    }
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
